8ba37ea5bda93753a0f3a16c87a6f9f4024c5ed5
YuDeokRin <ejrfls12@naver.com>
2022-01-24 12:30:40 +0900

    상품도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,23 @@
+package hello.itemservice.domain.item;
+
+
+import lombok.Data;
+
+@Data  //  @Data는 함부로 쓰는것은 위험하다. 이유는 예측하지 못하게 동작을 하기 때문에 조심히 써야한다.
+//@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price; //int를 쓰지않는 이유는 수량이 null로 값이 들어갈 수 도있기 때문이다. int를 쓸 경우  0이라도 들어가야된다.
+    private Integer quantity;  //int를 쓰지않는 이유는 수량이 null로 값이 들어갈 수 도있기 때문이다. int를 쓸 경우  0이라도 들어가야된다.
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,47 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ItemRepository {
+    /**
+     * 실무에서는 HashMap<> 사용 X  , Why? 동시에 여러 쓰레드가 접근할 떄(멀티쓰레드)가 store에 한번에 접근하게되면 안된다.
+     * ItemRepository는  singleton 으로 생성되기 때문이다.
+     * 해결 : ConcurrentHashMap<>, AtomicLong()을 사용하면된다.
+     */
+
+    private static final Map<Long, Item> store = new HashMap<>();  // static
+    private static long sequence = 0L ; // static
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id){
+        return store.get(id);
+    }
+
+    public List<Item> findAll(){
+        //store를 바로 collcation으로 반환하여도되는데 ArrayList로 감싸서 반환하게 되면, ArrayList에 값을 넣어도 store에 영향을 주지않기 떄문에 감싸서 반환한다.
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam){
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+    public void clearStore(){
+        store.clear();
+    }
+}
